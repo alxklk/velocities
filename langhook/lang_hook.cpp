1e56bf22ecfc70051b6df56f5a018856b2d2567c
Alex Kluchikov <klkspa@ukr.net>
2020-03-12 00:40:57 +0200

    WIP hook for some keys: «»°—

--- langhook/lang_hook.cpp
+++ langhook/lang_hook.cpp
@@ -2,10 +2,31 @@
 #include <winuser.h>
 #include <stdio.h>
 
+
+void unalt()
+{
+	INPUT inp;
+	inp.type=INPUT_KEYBOARD;
+	inp.ki.wVk=VK_MENU;
+	inp.ki.wScan=0;
+	inp.ki.dwFlags=KEYEVENTF_KEYUP;
+	inp.ki.time=0;
+	inp.ki.dwExtraInfo=0;
+	int res=SendInput(1,&inp,sizeof(inp));
+	printf("UnAlt = %i\n", res);
+}
+
 LRESULT CALLBACK HookCallback(int nCode, WPARAM wParam, LPARAM lParam)
 {
-	if((nCode==HC_ACTION)&&((wParam==WM_KEYDOWN)||(wParam==WM_SYSKEYDOWN)))
+	if((nCode==HC_ACTION)&&((wParam==WM_KEYDOWN)
+	//||(wParam==WM_SYSKEYDOWN)
+	))
 	{
+		bool winPressed=(GetKeyState(VK_RWIN)&0x8000);
+		bool altPressed=(GetKeyState(VK_LMENU)&0x8000);
+		bool shftPressed=(GetKeyState(VK_RSHIFT)&0x8000);
+		bool meta=shftPressed;
+
 		PKBDLLHOOKSTRUCT kbdstruct=(KBDLLHOOKSTRUCT*)lParam;
 		if(kbdstruct->vkCode==VK_PAUSE)
 		{
@@ -13,29 +34,34 @@ LRESULT CALLBACK HookCallback(int nCode, WPARAM wParam, LPARAM lParam)
 			PostMessage(GetForegroundWindow(), WM_INPUTLANGCHANGEREQUEST, 2, 0);
 			return 1;
 		}
-		else if((kbdstruct->flags&(1<<5))&&(kbdstruct->vkCode==VK_OEM_MINUS))
+
+		// printf("%02x f:%02x\n", kbdstruct->vkCode, kbdstruct->flags);
+		// if(kbdstruct->flags&(1<<5)) // Alt pressed
+		if(meta)
 		{
-			printf("ALT MINUS\n");
-			PostMessage(GetForegroundWindow(), WM_KEYDOWN, 'a', 1);
-			PostMessage(GetForegroundWindow(), WM_KEYDOWN, 'A', 1);
-			return 1;
-		}
-		else if(kbdstruct->vkCode==VK_OEM_MINUS)
-		{
-			printf("MINUS\n");
-			SendMessage(GetForegroundWindow(), WM_KEYDOWN, 'a', 1);
-			SendMessage(GetForegroundWindow(), WM_KEYDOWN, 'A', 1);
-			PostMessage(GetForegroundWindow(), WM_KEYDOWN, 'a', 1);
-			PostMessage(GetForegroundWindow(), WM_KEYDOWN, 'A', 1);
-			SendMessage(GetForegroundWindow(), WM_CHAR, 'a', 1);
-			SendMessage(GetForegroundWindow(), WM_CHAR, 'A', 1);
-			PostMessage(GetForegroundWindow(), WM_CHAR, 'a', 1);
-			PostMessage(GetForegroundWindow(), WM_CHAR, 'A', 1);
-			return 1;
-		}
-		else
-		{
-			printf("%02x f:%02x\n", kbdstruct->vkCode, kbdstruct->flags);
+			int subst=-1;
+			switch(kbdstruct->vkCode)
+			{
+				case VK_OEM_MINUS: subst=0x2014;break;
+				case '5': subst=0x00b0;break;
+				case VK_OEM_COMMA: subst=0x00ab;break;
+				case VK_OEM_PERIOD: subst=0x0bb;break;
+				default:;
+			}
+			if(subst!=-1)
+			{
+					INPUT inp;
+					inp.type=INPUT_KEYBOARD;
+					inp.ki.wVk=0;
+					inp.ki.wScan=subst;
+					inp.ki.dwFlags=KEYEVENTF_UNICODE;
+					inp.ki.time=0;
+					inp.ki.dwExtraInfo=0;
+					//unalt();
+					int res=SendInput(1,&inp,sizeof(inp));
+					printf("SendInput = %i\n", res);
+					return 1;
+			}
 		}
 	}
 	return CallNextHookEx(0, nCode, wParam, lParam);
