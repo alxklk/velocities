482d1ba1cda21f33ca9401dd7033633f5fe9bd77
Alex Kluchikov <klkspa@ukr.net>
2020-03-11 09:30:41 +0200

    WIP: Some experiments with lang_hook

--- langhook/lang_hook.cpp
+++ langhook/lang_hook.cpp
@@ -1,15 +1,41 @@
 #include <windows.h>
 #include <winuser.h>
+#include <stdio.h>
 
 LRESULT CALLBACK HookCallback(int nCode, WPARAM wParam, LPARAM lParam)
 {
-	if((nCode==HC_ACTION)&&(wParam==WM_KEYDOWN))
+	if((nCode==HC_ACTION)&&((wParam==WM_KEYDOWN)||(wParam==WM_SYSKEYDOWN)))
 	{
 		PKBDLLHOOKSTRUCT kbdstruct=(KBDLLHOOKSTRUCT*)lParam;
 		if(kbdstruct->vkCode==VK_PAUSE)
 		{
+			puts("SW");
 			PostMessage(GetForegroundWindow(), WM_INPUTLANGCHANGEREQUEST, 2, 0);
 			return 1;
+		}
+		else if((kbdstruct->flags&(1<<5))&&(kbdstruct->vkCode==VK_OEM_MINUS))
+		{
+			printf("ALT MINUS\n");
+			PostMessage(GetForegroundWindow(), WM_KEYDOWN, 'a', 1);
+			PostMessage(GetForegroundWindow(), WM_KEYDOWN, 'A', 1);
+			return 1;
+		}
+		else if(kbdstruct->vkCode==VK_OEM_MINUS)
+		{
+			printf("MINUS\n");
+			SendMessage(GetForegroundWindow(), WM_KEYDOWN, 'a', 1);
+			SendMessage(GetForegroundWindow(), WM_KEYDOWN, 'A', 1);
+			PostMessage(GetForegroundWindow(), WM_KEYDOWN, 'a', 1);
+			PostMessage(GetForegroundWindow(), WM_KEYDOWN, 'A', 1);
+			SendMessage(GetForegroundWindow(), WM_CHAR, 'a', 1);
+			SendMessage(GetForegroundWindow(), WM_CHAR, 'A', 1);
+			PostMessage(GetForegroundWindow(), WM_CHAR, 'a', 1);
+			PostMessage(GetForegroundWindow(), WM_CHAR, 'A', 1);
+			return 1;
+		}
+		else
+		{
+			printf("%02x f:%02x\n", kbdstruct->vkCode, kbdstruct->flags);
 		}
 	}
 	return CallNextHookEx(0, nCode, wParam, lParam);
