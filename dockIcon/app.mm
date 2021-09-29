5f3943c93bc806f076cc18967aedaf39831e0916
Oleksii Kluchikov <oleksii.kluchikov@avid.com>
2021-09-29 12:16:55 +0300

    Some macos dock drawing

--- dockIcon/app.mm
+++ dockIcon/app.mm
@@ -5,33 +5,39 @@
 
 int main()
 {
-  [NSAutoreleasePool new];
-  id app = [NSApplication sharedApplication];
-   [app setActivationPolicy:NSApplicationActivationPolicyRegular];
-  
-  NSRect frame = NSMakeRect(0, 0, 300, 300);
-  id window = 
-    [[[NSWindow alloc] initWithContentRect:frame
-                                 styleMask:NSWindowStyleMaskTitled
-                                   backing:NSBackingStoreBuffered
-                                     defer:NO] autorelease];
-  [window cascadeTopLeftFromPoint:NSMakePoint(10, 10)];
-  [window setTitle:@"Hello"];
-  [window makeKeyAndOrderFront:nil];
+	[NSAutoreleasePool new];
+	id app = [NSApplication sharedApplication];
+	[app setActivationPolicy:NSApplicationActivationPolicyRegular];
+	
+	NSRect frame = NSMakeRect(0, 0, 300, 300);
+	id window = [
+			[[NSWindow alloc]
+			initWithContentRect:frame
+			styleMask:NSWindowStyleMaskTitled
+			backing:NSBackingStoreBuffered
+			defer:NO]
+			autorelease
+		];
+	[window cascadeTopLeftFromPoint:NSMakePoint(10, 10)];
+	[window setTitle:@"Hello"];
+	[window makeKeyAndOrderFront:nil];
 
-  id button = [[[NSButton alloc] initWithFrame:frame] autorelease];
-  [button setTarget:app];
-  [button setAction:@selector(terminate:)];
-  [button setTitle:@"Quit"];
-  [[window contentView] addSubview:button];
+	id button = [[[NSButton alloc] initWithFrame:frame] autorelease];
+	[button setTarget:app];
+	[button setAction:@selector(terminate:)];
+	[button setTitle:@"Quit"];
+	[[window contentView] addSubview:button];
 
-	[app dockTile].showsApplicationBadge=true;
-	[app dockTile].badgeLabel=@"Scinc";
+//	[app dockTile].showsApplicationBadge=true;
+//	[app dockTile].badgeLabel=@"Scinc";
 
-	[app dockTile].contentView=[[[DockIcon alloc]initWithFrame:NSMakeRect(0,0,128,128)]autorelease];
+	[app dockTile].contentView=[
+			[[DockIcon alloc]initWithFrame:NSMakeRect(0,0,128,128)]
+			autorelease
+		];
 	[[app dockTile] display];
-  [app activateIgnoringOtherApps:YES];
-    [app setActivationPolicy:NSApplicationActivationPolicyRegular];
-[app run];
-  return 0;
+	[app activateIgnoringOtherApps:YES];
+	[app setActivationPolicy:NSApplicationActivationPolicyRegular];
+	[app run];
+	return 0;
 }
--- dockIcon/dockIcon.mm
+++ dockIcon/dockIcon.mm
@@ -7,15 +7,28 @@
 	NSGraphicsContext* gc = [NSGraphicsContext currentContext];
 	[gc saveGraphicsState];
 
-	[[NSColor blackColor] setStroke];
-	[[NSColor greenColor] setFill];
+	NSBezierPath* path = [NSBezierPath bezierPath];
+	// [path appendBezierPathWithOvalInRect: NSMakeRect(10, 10, 100, 100)];
 
-	NSRect rect = NSMakeRect(10, 10, 100, 100);
-	NSBezierPath* circlePath = [NSBezierPath bezierPath];
-	[circlePath appendBezierPathWithOvalInRect: rect];
+	[path appendBezierPathWithRoundedRect: NSMakeRect(10, 10, 108, 108)
+		xRadius:10
+		yRadius:10
+	];
+	[path moveToPoint: NSMakePoint(64,64)];
+	[path relativeCurveToPoint: NSMakePoint(0,-20)
+		controlPoint1:NSMakePoint(20,0)
+		controlPoint2:NSMakePoint(20,-20)
+	];
+	[path relativeCurveToPoint: NSMakePoint(0,20)
+		controlPoint1:NSMakePoint(-20,0)
+		controlPoint2:NSMakePoint(-20,20)
+	];
 
-	[circlePath stroke];
-	[circlePath fill];
+	[[NSColor whiteColor] setStroke];
+	[[NSColor colorWithRed:0.5 green:0.8 blue: 0.6 alpha: 1.0] setFill];
+	[path setLineWidth:10];
+	[path stroke];
+	[path fill];
 
 	[gc restoreGraphicsState];
 }
